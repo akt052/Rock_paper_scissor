f7b99b71046071c9d7df9308dc69ad1310406993
DESKTOP-UKJ7IJD <akshat0akt52@gmail.com>
2024-04-11 23:10:45 +0530

    skeleton of rps-ui

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    return "paper";
+}
+
+let computerSelection = getComputerChoice().toLowerCase();
+let playerSelection = "rock";
+
+function playRound(playerSelection, computerSelection){
+    return "You Lose! Paper beats Rock";
+}
+
+function playGame(){
+
+}
